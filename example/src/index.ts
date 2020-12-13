759d74a9992173dcc96cd3749004d6029cebff1b
Jan Kuß <kuss@hmi-logic.com>
2020-12-13 21:11:10 +0100

    Reset example to master

--- example/src/index.ts
+++ example/src/index.ts
@@ -6,8 +6,6 @@ import {
   Avatar,
   Shroom,
   loadRoomTexture,
-  WallFurniture,
-  Landscape,
 } from "@jankuss/shroom";
 import { DummyRoom } from "./DummyRoom";
 
@@ -32,16 +30,10 @@ PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 const shroom = Shroom.create({ application, resourcePath: "./resources" });
 const room = Room.create(shroom, {
   tilemap: `
-    xxx1111111
-    xxx1111111
-    xxx1111111
-    xxx1111111
-    xxx0000000
-    0000000000
-    0000000000
-    0000000000
-    0000000000
-  `,
+    0000
+    0000
+    0000
+   `,
 });
 
 const avatar = new Avatar({
@@ -52,38 +44,24 @@ const avatar = new Avatar({
   roomZ: 0,
 });
 
-room.x = application.screen.width / 2 - room.roomWidth / 2;
-room.y = application.screen.height / 2 - room.roomHeight / 2;
+room.x = 100;
+room.y = 200;
 
 room.wallTexture = loadRoomTexture("./images/tile.png");
 room.floorTexture = loadRoomTexture("./images/tile.png");
 room.wallColor = "#dbbe6e";
 room.floorColor = "#eeeeee";
 
-const wallFurniture1 = new WallFurniture({
-  roomX: 0,
-  roomY: 5,
+const furniture = new FloorFurniture({
+  roomX: 2,
+  roomY: 0,
+  direction: 2,
   roomZ: 0,
-  direction: 2,
-  type: "window_skyscraper",
-});
-
-const wallFurniture2 = new WallFurniture({
-  roomX: 0,
-  roomY: 5,
-  roomZ: 2,
-  direction: 2,
-  type: "window_skyscraper",
+  type: "exe_table",
+  animation: "1",
 });
 
 room.addRoomObject(avatar);
-room.addRoomObject(wallFurniture1);
-room.addRoomObject(wallFurniture2);
+room.addRoomObject(furniture);
+
 application.stage.addChild(room);
-
-const landscape = new Landscape();
-landscape.leftTexture = loadRoomTexture("./images/a1.png");
-landscape.rightTexture = loadRoomTexture("./images/a2.png");
-landscape.color = "#cccccc";
-
-room.addRoomObject(landscape);
